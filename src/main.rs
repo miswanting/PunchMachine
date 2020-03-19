26a749633b726c421d37bb6a1c51e245df9616f5
(ง •_•)ง <miswanting@users.noreply.github.com>
2020-03-19 23:46:38 +0800

    GO

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::path::Path;
+use std::thread
+use chrono::prelude::*;
+use std::fs::OpenOptions;
+
+
 fn main() {
-    println!("Hello, world!");
+    // 启动检查
+    if Path::new("tmp").exists() {
+        // 读→写
+        let data = fs::read_to_string("tmp").unwarp()
+        let file = OpenOptions::new().append(true).open("report.txt").unwrap()
+    }
+    write_to_report("",data)
+    // 写入现在
+    // 轮询
+    loop{
+        thread::sleep_ms(1000*60)
+        let dt= Utc::now();
+        let tmp=dt.format("%y%m%d-%H%M").to_string()
+        println!("{:?}",tmp)
+    }
+}
+fn read_from_tmp(){ 
+
 }
